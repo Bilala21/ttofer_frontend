a02e04edcbe9e91f65a518f6cc0ab237ffb8c712
Bilal <bilal.email>
2024-11-04 16:51:21 +0500

    offer modal working

--- src/app/components/modals/make-offer-modal/make-offer-modal.component.ts
+++ src/app/components/modals/make-offer-modal/make-offer-modal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-make-offer-modal',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './make-offer-modal.component.html',
   styleUrl: './make-offer-modal.component.scss'
 })
