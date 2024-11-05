005ed8201a44774ad4b86e688f9488e7d7e29eee
Bilal <bilal.email>
2024-11-05 10:02:42 +0500

    BILAL:change in make offer modal

--- src/app/components/modals/make-offer-modal/make-offer-modal.component.ts
+++ src/app/components/modals/make-offer-modal/make-offer-modal.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-make-offer-modal',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule,NgIf],
   templateUrl: './make-offer-modal.component.html',
-  styleUrl: './make-offer-modal.component.scss'
+  styleUrls: ['./make-offer-modal.component.scss'] // Corrected to `styleUrls`
 })
 export class MakeOfferModalComponent {
+  showConfirmModal: boolean = false;
+  offerForm: FormGroup;
+  router = inject(Router); // Correct way to inject Router in standalone component
 
+  constructor(private fb: FormBuilder) {
+    this.offerForm = this.fb.group({
+      offer_price: ['', [Validators.required]],
+      description: [''],
+    });
+  }
+
+  handleFirstStep() {
+    this.showConfirmModal = !this.showConfirmModal;
+  }
+
+  onSubmit(): void {
+    this.showConfirmModal = !this.showConfirmModal;
+    if (this.offerForm.valid) {
+      // Handle form submission, e.g., navigate or send data to the server
+      console.log('Form submitted successfully', this.offerForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
