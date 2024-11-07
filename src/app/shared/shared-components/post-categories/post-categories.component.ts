ba3cd45a2b964bb1e1a3fa3ec58eceea03239535
Bilal <bilal.email>
2024-11-07 10:09:15 +0500

    changes

--- src/app/shared/shared-components/post-categories/post-categories.component.ts
+++ src/app/shared/shared-components/post-categories/post-categories.component.ts
@@ -11,8 +11,8 @@ export class PostCategoriesComponent implements OnInit {
   constructor(private globalStateService: GlobalStateService) { }
   ngOnInit() {
     this.globalStateService.currentState.subscribe((state) => {
-      debugger
-      this.categories = [...state.categories, { color: "#fff7eb", ImgSrc: '/assets/catImage/bit-coin.png', name: 'Crypto Market', subTitle: 'Coming Soon' }]
+      // debugger
+      this.categories = [...state.categories, state.categories.length && { color: "#fff7eb", ImgSrc: '/assets/catImage/bit-coin.png', name: 'Crypto Market', subTitle: 'Coming Soon' }]
     })
   }
 
