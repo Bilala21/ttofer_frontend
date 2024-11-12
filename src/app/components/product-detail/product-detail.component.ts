0734c8376071f841f48700faa5fb134dac279986
Bilal <bilal.email>
2024-11-12 15:14:14 +0500

    Revert "Merge branch 'ttoffer-final-tasks' of https://github.com/Bilala21/ttofer_frontend into production"
    This reverts commit 370214503c060dcd77900dec64d7ab2a330e8bb4, reversing
    changes made to 47f282a63c105f954390788c7edf114f7ac58f61.

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -76,11 +76,9 @@ export class ProductDetailComponent implements OnInit {
     this.loading = true
     this.mainServices.getProductById({ product_id: this.productId }).subscribe({
       next: (value) => {
-        this.product = value.data;
+        this.product = value.data
         this.attributes = JSON.parse(value.data.attributes)
         this.loading = false
-        this.productView()
-
         console.log(JSON.parse(value.data.attributes), 'attributes');
       },
       error: (err) => {
@@ -91,7 +89,6 @@ export class ProductDetailComponent implements OnInit {
   }
 
   productView() {
-    debugger
     const productViewDetail = {
       product_id: this.productId,
       user_id: this.currentUserid
