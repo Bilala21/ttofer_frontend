a64c560741d956fb80e316fc66a99264b3d027cb
Bilal <bilal.email>
2024-11-15 11:27:26 +0500

    notifiction page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -234,6 +234,7 @@ export class HeaderNavigationComponent implements OnInit {
       next: (res: any) => {
         this.categories = res.data;
         this.loading = false;
+        console.log(res,"test12");
         this.globalStateService.setCategories(res.data);
       },
       error: (err) => {
