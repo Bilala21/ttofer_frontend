db11fdfd2b3e7b5a087b43f639168fa8765f15ff
unknown <haroonrana2186123@gmail.com>
2024-11-25 16:41:28 +0500

    profile module

--- src/app/profilemodule/profile-sidebar/profile-sidebar.component.ts
+++ src/app/profilemodule/profile-sidebar/profile-sidebar.component.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { Extension } from '../../helper/common/extension/extension';
 import { MainServicesService } from '../../shared/services/main-services.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { NgClass } from '@angular/common';
-
+import { NgClass, NgFor } from '@angular/common';
+import { sideBarItems } from './json-data';
 @Component({
   selector: 'app-profile-sidebar',
   templateUrl: './profile-sidebar.component.html',
   styleUrls: ['./profile-sidebar.component.css'],
   standalone: true,
-  imports:[NgClass]
+  imports:[NgClass,NgFor]
 })
 export class ProfileSidebarComponent implements OnInit {
   selectedTab!: string;
   selectedTabItem: string = '';
+  sidebarItems:any=[]
   constructor(
     private mainServices: MainServicesService,
     private extension: Extension,
     private http: HttpClient
-  ) {}
+  ) {
+    this.sidebarItems=sideBarItems
+  }
 
   ngOnInit() {
     if (this.selectedTabItem != null) {
